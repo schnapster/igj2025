e8bd342bd191c998d823af2b2dafd276e1a83b2c
Schnapster <napster@npstr.space>
2025-11-08 14:43:24 +0100

    Reduce Enemy Speed

--- src/main/kotlin/dev/capybaralabs/igj2025/elements/Enemy.kt
+++ src/main/kotlin/dev/capybaralabs/igj2025/elements/Enemy.kt
@@ -9,6 +9,7 @@ import kotlin.math.min
 
 class EnemyEntity(
 	val position: Vector2 = kvector2(getScreenWidth() / 2, getScreenHeight() - 200),
+	val initialSpeed: Float = 200f,
 	directionAiInput: DirectionAiComponent = DirectionAiComponent(),
 ) : Entity() {
 	companion object {
@@ -23,7 +24,7 @@ class EnemyEntity(
 		//movement
 		addComponent(SimplePositionComponent(position))
 		addComponent(DirectionComponent(kvector2(0, 0)))
-		addComponent(SpeedComponent(400f))
+		addComponent(SpeedComponent(initialSpeed))
 		addComponent(
 			BorderComponent(
 				marginTop = ((ENEMY_TEXTURE.height / 2f) * scale).toFloat() + 50f,
